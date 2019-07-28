45ba30f348174688fa1ec5798f864f45119fce6c
Fotoz <fotozxx@gmail.com>
2019-07-28 13:10:34 +0300

    s-footer + scroll

--- js/common.js
+++ js/common.js
@@ -8,4 +8,34 @@ $(function() {
 		autoHeight: true
 	});
 
+
+// smooth-scroll
+	$('[data-scroll]').on('click', function(event) {
+		event.preventDefault();
+
+		var sectionId = $(this).data('scroll'),
+				sectionOffset = $(sectionId).offset().top;
+
+		// нужно для изменения активной ссылки
+		// $('.nav a').removeClass('active'); // #nav
+		// $(this).addClass('active');
+
+		$('html, body').animate({
+			scrollTop: sectionOffset
+		}, 1000);
+	});
+
+
+// toTop-button
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height() - 1) {
+				$('.toTop').addClass('toTop-active');
+		} else {
+				$('.toTop').removeClass('toTop-active');
+		};
+	});
+	$('.toTop').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
+
 });
